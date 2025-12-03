ff7cbbfacb7f8c28007404caf56ce63815cebe9d
alba <albamalca15@gmail.com>
2025-12-03 05:09:26 -0500

    feat: update CORS configuration to allow multiple frontend origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,12 @@ async function bootstrap() {
   
   // Habilita CORS para el frontend
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin:  [
+    process.env.FRONTEND_URL || 'http://localhost:3000',
+    'http://localhost:3001',
+      'http://localhost:3002',
+      'http://localhost:5173',
+    ],
     credentials: true,
   });
 
